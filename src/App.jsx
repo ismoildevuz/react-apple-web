69ec82085361af233e4add13312563bd046566b1
ismoildevuz <ismoildevuz@gmail.com>
2024-08-14 13:00:30 +0500

    feat: init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/HighLights";
+
+const App = () => {
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
+};
+
+export default App;
