ffe4d33d80c82f2d721b484640cbbb8ccd064677
ismoildevuz <ismoildevuz@gmail.com>
2024-08-20 17:07:06 +0500

    fix: build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import Highlights from "./components/HighLights";
+import Highlights from "./components/Highlights";
 
 const App = () => {
   return (
